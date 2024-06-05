338cbb7abdc52d135dfefbe0c95073e1312a274e
Bhaskar Gogoi <bhaskar@foodieverseindia.com>
2024-06-05 09:56:13 +0000

    The massive dropdown form

--- src/components/DropdownResource.tsx
+++ src/components/DropdownResource.tsx
@@ -1,13 +1,15 @@
 import { FC, useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
-import { DropdownOpts, Option, DropdownFormOpts } from '../App.type';
+import { DropdownOpts, Option, DropdownFormOpts, ToggleState, ToggleCore, ToggleChildren, Component } from '../App.type';
 import CustomList from '../core/CustomList';
-import CustomOptionForm, { FoodieText, FoodieCheckbox } from '../core/CustomOptionForm';
-import Dropdown from '../core/Dropdown';
+import FoodieText from '../core/FoodieText';
+import ToggleAction, { BASE_TA_ROW_DROPDOWN } from '../components/ToggleAction';
+import CustomOptionForm from '../core/CustomOptionForm';
 
 const CASCADE_OPTIONS: Option[] = [
-  { value: 'brand-category', name: 'Brand Category' },
-  { value: 'global', name: 'Global' }
+  // { value: 'brand-category', name: 'Brand Category' }
 ];
 
 export const BASE_DROPDOWN: DropdownFormOpts = {
@@ -17,29 +19,96 @@ export const BASE_DROPDOWN: DropdownFormOpts = {
     callbackFn: (obj: Object) => {
       alert(JSON.stringify(obj));
     }
-} 
+}
 
-const DropdownResource: FC<DropdownFormOpts> = ({ name, cascade, cascadeOptions, defaultValue, options, callbackFn }) => {
+async function fetchUIResource(uiType: string, id: string) {  
+  return axios.get(`https://4ccsm42rrj.execute-api.ap-south-1.amazonaws.com/dev/foodie-api?uiType=${uiType}&id=${id}`, {
+      headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJoYXNrYXIiLCJuYW1lIjoiQmhhc2thciBHb2dvaSIsInR5cGUiOiJzdXBlciIsInZhbHVlIjoiMDAwMDAwIiwiaWF0IjoxNzE1ODQ4Mzc0fQ.DArYQmB65k3-OIBkHDmIKbPLIFVqlfBg0VkOOgp3zVs'
+      }
+  })
+}
 
-    const [ddnOptions, setDdnOptions] = useState<Option[]>(options || []);
-    const [valid, setValid] = useState(false);
+const DropdownResource: FC<DropdownFormOpts> = ({ name, cascade, cascadeOptions, defaultValue, options, callbackFn, readOnly }) => {
 
-    const [dropdownName, setDropdownName] = useState(name);
-    const [cascadeType, setCascadeType] = useState(cascade || 'global');
-      
+  // dev
+  const borderOn = false;
+  // const borderOn = true;
+
+  const [ddnOptions, setDdnOptions] = useState<Option[]>(options || []);
+  const [valid, setValid] = useState(false);
+
+  const [dropdownName, setDropdownName] = useState(name);
+  const [selectedCascade, setSelectedCascade] = useState(cascade || '');
+  const [cascadeType, setCascadeType] = useState(cascade || 'global');
+  const [visibility, setVisibility] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  
+
+  const visibilityToggle: ToggleCore = {
+    fieldName: 'cascade',
+    toggleName: 'Visibility',
+    state: cascade == 'global' || !cascade ? ToggleState.Off: ToggleState.On,
+    info: 'is part of cascade dropdown',
+    readOnly: !!readOnly || !!name,
+    onToggleChange: (isOn: boolean) => {
+      alert(`Visibility: ${isOn}`);
+      setVisibility(isOn);
+    }
+  }
+
+  // const [cascades, setCascades] = useState<Option[]>(cascadeOptions || [{ value: '', name: ' --- please select --- '}]);
+
+  const cascadeChildren = {
+    on: {
+      component: Component.DROPDOWN,
+      opts: {
+        options: cascadeOptions,
+        name: 'Cascade',
+        // readOnly: !!readOnly,
+        ...(cascade !== 'global' && { selectedValue: cascade })
+      },
+      selectedCallback: function (valueObj: any) {  
+        let val = valueObj.value;
+        alert(`Cascade chosen : ${val}`);
+        setCascadeType(val);
+      }
+    }
+  }
+
+  const [children, setChildren] = useState<ToggleChildren>(cascadeChildren);
+
+  const { isPending, isFetching, error, data } = useQuery({
+    queryKey: ['dropdown', 'cascade'],
+    queryFn: async () => {
+        const data = await fetchUIResource('dropdown', 'cascade');
+        alert(JSON.stringify(data.data.result.options))
+        return data.data;
+    },
+    staleTime: Infinity,
+    enabled: !name
+  });
+
+  // alert(`Cascade: ${cascade}`);
 
   const deleteOption = (value: string) => {
-    alert(`Deleting ${value}`)
-    // setOptions(options.filter(o => o.value !== value));
+    // alert(`Deleting ${value}`)
+    setDdnOptions(ddnOptions.filter(o => o.value !== value));
   }
 
   const addToList = (name: string, value: string) => {
 
-    const names = options.map(o => o.name);
-    const values = options.map(o => o.value);
+    const names = ddnOptions.map(o => o.name);
+    const values = ddnOptions.map(o => o.value);
 
-    if (!names.includes(name) && !values.includes(value)) {
-      setDdnOptions([...options, { name, value }]);
+    if (!names.includes(name.trim()) && !values.includes(value.trim())) {
+      if (name.trim() == value.trim()) {
+        let tokens = value.trim().split(' ');
+        let valueString = tokens.map(t => t.toLowerCase()).join('-');
+        setDdnOptions([...ddnOptions, { name: name.trim(), value: valueString }]);
+      } else {
+        setDdnOptions([...ddnOptions, { name: name.trim(), value: value.trim() }]);
+      }
     } else {
       if (names.includes(name)) {
         alert(`Option name [${name}] already exists`);
@@ -52,105 +121,110 @@ const DropdownResource: FC<DropdownFormOpts> = ({ name, cascade, cascadeOptions,
   }
 
   useEffect(() => {
-    if (dropdownName && cascadeType) {
-      setValid(true);
+    alert(`Visibility[${visibility}] :: cascadeType[${cascadeType}]`);
+    if (dropdownName && cascadeType && dropdownName.trim() && ddnOptions.length) {
+      let validOptions = (visibility && cascadeType !== 'global') || !visibility;
+
+      alert(`validOptions ${validOptions}`)
+      setValid(validOptions);
+    } else {
+      setValid(false);
     }
 
-  }, [dropdownName, cascadeType])
+  }, [dropdownName, cascadeType, ddnOptions, visibility]);
 
+  useEffect(() => {
+    if (isFetching) {
+      alert(`IsPending: ${isFetching}`)
+      setIsLoading(true)
+    } else if (!isPending) {
+      setIsLoading(false);
 
-    return (<div className='container mx-auto bg-white rounded-lg border border-blue-700'>
-      <div className="border-gray-900/10 w-full min-w-full text-center">
-        <h2 className="text-xl uppercase font-semibold leading-7 text-gray-500">{!dropdownName ? 'New': 'Edit'} Dropdown</h2>
-        <p className="mt-1 text-sm leading-6 text-gray-600">
-          Provide details to create a new dropdown resource either as a part of cascade hierarchy or global
-        </p>
-      </div>
-      <div className="border-gray-900 w-full h-full flex flex-row flex-wrap">
-        <main role="main" className="w-full sm:w-2/3 md:w-3/5 pt-1 px-12 border">
-          <form className='ml-2 pe-40 border space-y-12 border-red-700'>
-            <div className="border border-green-800">
-              <FoodieText label='Name' fieldName='name' action={setDropdownName} value={dropdownName}/>
+      if (error) {
+        alert(error.response.data);
+        if (error.response && error.response.status == '404') {
+        }
+      } else {
+        if (data.result && data.result.options) {
+          const { on } = children;
+
+          const { opts } = on;
+
+          alert(typeof opts.selectedCallback);
+
+          setChildren({
+            ...children,
+            on: {
+              ...on,
+              opts: {
+                ...opts,
+                options: [
+                  { value: '', name: ' --- please select --- '},
+                  ...data.result.options
+                ],
+                selectedCallback: function (valueObj: any) {  
+                  let val = valueObj.value;
+                  alert(`Cascade chosen : ${val}`);
+                  setCascadeType(val);
+                }
+              }
+            }
+          });
+        }
+      }  
+    }
+  }, [isPending, isFetching, error, data]);
+
+    return (<div className=' bg-white rounded-lg border border-gray-400 py-10 h-max'>
+      <div className={`${borderOn ? 'border-red-800': ''} mt-10 flex flex-row min-h-10 min-w-max justify-between`}>
+
+        <main role="main" className={`${borderOn ? 'border border-yellow-500': ''} basis-4/6 max-w-[600px]`}>
+
+          <form className={`${borderOn ? 'border border-red-700': ''} ml-10 pe-10 space-y-8 `}>
+            <div className={`${borderOn ? 'border border-green-800' : ''}`}>
+              <FoodieText label='Name' fieldName='name' action={setDropdownName} value={dropdownName} size='w-full'
+                readOnly={!!readOnly}
+              />
             </div>
-
-            <div className="border border-green-800">
-              <label htmlFor="visibility" className="block text-sm font-medium leading-6 text-gray-900">
-                Visibility
-              </label>
-              <div className="mt-2">
-                <div className="flex items-center mb-4">
-                    <input id="visibility-global" type="radio" value="global" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label htmlFor="default-radio-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Global</label>
-                </div>
-                <div className="flex items-center">
-                    <input checked id="default-radio-2" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label htmlFor="default-radio-2" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
-                </div>
+            <div className={`${borderOn ? 'border border-green-800' : ''}`}>
+              <ToggleAction toggle={visibilityToggle}
+                children={children} isLoading={isLoading} linkedExternalVal={cascadeOptions}/>
+            </div>
+            {!readOnly && <><div className={`${borderOn ? 'border border-green-800' : ''}`}>
+              <CustomOptionForm action={addToList} />
+            </div>
+            <div className={`${borderOn ? 'border border-blue-900' : ''} pe-10`}>
+              <div className='inline-flex gap-2 flex-row w-full'>
+                <button 
+                  type='button' 
+                  disabled={!valid}
+                  onClick={() => callbackFn({
+                    dropdownName,
+                    cascadeType,
+                    visibility,
+                    ddnOptions
+                  })}
+                  className={`py-2.5 px-6 text-sm rounded-md uppercase
+                      ${valid 
+                        ? 'cursor-pointer text-indigo-500  bg-indigo-50 transition-all duration-500 hover:bg-indigo-100'
+                        : 'cursor-not-allowed text-gray-300 bg-gray-100 '}
+                      font-semibold text-center shadow-xs `}>
+                      {name ? 'Update': 'Create' } Dropdown
+                  </button>
               </div>
             </div>
+            </>}
           </form>
         </main>
-        <aside className="w-full sm:w-1/3 md:w-2/5 px-10 py-10 min-h-screen/2 border"></aside>
-      </div>
+        <aside className=" basis-2/6">
+          {ddnOptions.length > 0 && 
+            <div className="sticky top-0 p-4 w-full h-full rounded-lg border-l border-pink-500">
+              <CustomList options={ddnOptions} optionAction={deleteOption} readOnly={readOnly} />
+            </div>
+          }
+        </aside>
+      </div> 
     </div>);
 }
 
 export default DropdownResource;
-
-/* 
-<div className="container mx-auto bg-white rounded-lg border border-blue-700">
-    <div className="flex flex-row flex-wrap py-4">
-      <main role="main" className="w-full sm:w-2/3 md:w-3/5 pt-1 px-12">
-        <form className='ml-2 pe-40'>
-          <div className="space-y-12">
-            <div className="border-gray-900/10 pb-12 bg-red-50">
-              <h2 className="text-xl font-semibold leading-7 text-gray-900">{!dropdownName ? 'New': 'Edit'} Dropdown</h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                Provide details to create a new dropdown resource either as a part of cascade hierarchy or global
-              </p>
-
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4">
-                <div className="sm:col-span-3">
-                    <FoodieText label='Dropdown Name' fieldName='name' action={setDropdownName} value={dropdownName}/>
-                </div>
-
-                <div className="sm:col-span-3">
-                    <Dropdown options={cascadeOptions} name='Cascade' selectedValue={cascadeType}
-                      selectedCallback={(selected: Option) => {
-                        console.log('Selected cascade: ', selected);
-                        setCascadeType(selected.value);
-                      }}
-                    />
-
-                    <FoodieCheckbox label='Global' info='can be used by other assets' checkFn={setIsGlobal} checked={isGlobal} />
-                </div>
-                <div className='sm:col-span-3 mx-3 p-4 border rounded-lg'>
-                  <CustomOptionForm action={addToList} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </main>
-      <aside className="w-full sm:w-1/3 md:w-2/5 px-10 py-10 min-h-screen/2 border-l">
-          <div className="sticky top-0 p-4 w-full h-full bg-blue-50 rounded-lg">
-            <CustomList options={options} optionAction={deleteOption} />
-          </div>
-      </aside>
-    </div>
-    <div className='border border-blue-900 px-10'>
-      <div className='mb-8 inline-flex gap-2 mt-10 flex-row-reverse w-full'>
-                <button 
-                    type='button' 
-                    onClick={() => callbackFn({
-                      dropdownName,
-                      cascadeType
-                    })}
-                    className={`py-2.5 px-6 text-sm bg-indigo-50 rounded-full 
-                        ${valid ? 'cursor-pointer text-indigo-500': 'cursor-not-allowed text-indigo-300'}
-                        font-semibold text-center shadow-xs transition-all duration-500 hover:bg-indigo-100`}>
-                        Create Dropdown
-                </button>
-            </div>
-    </div>
-  </div> */
