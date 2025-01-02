77b8e89e53578b39f10dd94689660f5a9eb87746
Bhaskar Gogoi <bhaskar@foodieverseindia.com>
2025-01-02 08:49:46 +0000

    added wide option

--- src/core/Count.tsx
+++ src/core/Count.tsx
@@ -6,10 +6,11 @@ type CountProps = {
     array?: Array<any>,
     label: string,
     isLoading?: boolean,
-    isID?: boolean
+    isID?: boolean,
+    isWide?: boolean
 }
 
-const Count: FC<CountProps> = ({ count, label, isLoading, array, isID }) => {
+const Count: FC<CountProps> = ({ count, label, isLoading, array, isID, isWide }) => {
 
     const [number, setNumber] = useState(array ? array.length : count);
 
@@ -23,7 +24,7 @@ const Count: FC<CountProps> = ({ count, label, isLoading, array, isID }) => {
         
     }, [array, count])
 
-    return (<div className={`rounded h-40 w-60 flex flex-col ${isID ? 'border-4 border-gray-500': 'border border-gray-700 '} pt-5`}>
+    return (<div className={`rounded h-40 ${isWide ? 'w-80': 'w-60'} flex flex-col ${isID ? 'border-4 border-gray-500': 'border border-gray-700 '} pt-5`}>
         {!isLoading && <div className={`w-full inline-block text-7xl font-bold ${isID ? 'text-gray-500 font-serif' : ' text-indigo-700'} basis-3/4 text-center align-middle`}>{number}</div>}
         {isLoading && <div className='basis-3/4 text-center align-middle'><Loader /></div>}
         <div className={`rounded-l-none rounded-r-none w-full text-lg uppercase font-semibold ${isID ? 'bg-gray-500' : 'bg-gray-700'} 
